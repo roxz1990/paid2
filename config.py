c05732bde3e7b3cf5688c76411cd57a436a1271d
roxz1990 <roxzgaming56@outlook.com>
2024-12-22 14:06:50 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY 
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7734536339:AAE6iSco6c9k2ihbGRhdYJ5_43DdMLD-SeQ")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@Roxz_gaming")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7352008650")
+
+
+
+
+
+
+
+  
